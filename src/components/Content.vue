ae1a934c0972e45a002e32ff45648ecc5b99ee78
Leo <braian.tamay@hotmail.com.ar>
2019-06-26 20:49:46 -0300

    my five commit

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,5 +1,19 @@
 <template>
+  <div>
   <h1>{{movieTitle}}</h1>
+  <table class="table">
+  <thead>
+  </thead>
+  <tbody>
+    <tr v-for="movie in movies" :key="movie.id">
+      <th scope="row">{{movie.id}}</th>
+      <td>{{movie.name}}</td>
+      <td>{{movie.year}}</td>
+      <td>{{movie.age}}</td>
+    </tr>
+  </tbody>
+</table>
+</div>
 </template>
 
 <script>
@@ -11,5 +25,14 @@ props: ['movieTitle'],
       movies: []
     }
   },
+  mounted() {
+    this.loadCatalog()
+  },
+  methods: {
+    loadCatalog: async function() {
+      const data = await fetch('./catalog.json')
+      this.movies = await data.json()
+    }
+  }
 }
 </script>
